9ef9c5b012c93013ce6a0ee7a76f5e6052022ee9
Juan Bacardit Barrera <juanbacardit@hotmail.es>
2015-11-25 12:49:07 +0100

    a

--- Readme.md
+++ Readme.md
@@ -2,14 +2,20 @@
 
 To get up and running. Clone repository and execute following on terminal/cmd.exe:
 
-	1. npm install to download the node_modules
-	2. `npm test` to start test runner in watch mode to Spec folder.
+`npm install` 
+
+to download the node_modules, and 
+
+`npm test` 
+
+to start test runner in watch mode to Spec folder.
 
 Also, you can execute it on prompt. Inside de main folder execute
 
-	`** node_modules/.bin/jasmine-node .tests/ --autotest --growl --color **`
+`node_modules/.bin/jasmine-node .tests/ --autotest --growl --color`
 	
 More information about Jasmine a Sinon:
-	* [Jasmine.js](http://jasmine.github.io/)
-	* [Sinon.js](http://sinonjs.org)
+
+	· [Jasmine.js](http://jasmine.github.io/)
+	· [Sinon.js](http://sinonjs.org)
 	
--- src/HelloWorld.Module.js
+++ src/HelloWorld.Module.js
@@ -2,26 +2,26 @@
 
 var _ = require("underscore.string");
 
-var HW = (function(){
+var HW = (function () {
 	
 	//private members
 	var byeMessage = "Bye, %s"
 	var helloMessage = "Hello, %s"
-	
-	var putSomeExclamations = function(message){
-		return _.sprintf("%s!!!",message);
+
+	var putSomeExclamations = function (message) {
+		return _.sprintf("%s!!!", message);
 	};
 	
 	//public members
-	var constructor = function(){
-		 this.SayHello = function(name){
-			return putSomeExclamations(_.sprintf(helloMessage,name));
-		 };
-		 this.SayGoodBye = function(name){
-			 return _.sprintf(byeMessage, name);
-		 };
+	var constructor = function () {
+		this.SayHello = function (name) {
+			return putSomeExclamations(_.sprintf(helloMessage, name));
+		};
+		this.SayGoodBye = function (name) {
+			return _.sprintf(byeMessage, name);
+		};
 	};
 	return constructor;
 })();
 
-module.exports=HW;
+module.exports = HW;
